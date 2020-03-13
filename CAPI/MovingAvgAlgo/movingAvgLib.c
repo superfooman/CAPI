725e579ef1fa17e79219c63b3e501b902ea2c776
superfooman <bugaboo8834@hotmail.com>
2020-03-13 01:31:32 -0700

    Add moving average algo (C) code its source and header

---
+++ CAPI/MovingAvgAlgo/movingAvgLib.c
@@ -0,0 +1,39 @@
+#include "movingAvgLib.h"
+#include <stdlib.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    double movingAvgCal(int* movingWindowArr, double* ptrSum, int pos, int len, int nextNum)
+    {
+        *ptrSum = *ptrSum - movingWindowArr[pos] + nextNum;
+        movingWindowArr[pos] = nextNum;
+        return *ptrSum / len;
+    }
+
+    DECLDLL void MovingAvgArr(int* inputArr, int inputArrLength, int* movingWindowArr, int movingWindow, double* outputArry)
+    {
+        int pos = 0;
+        double sum = 0;
+
+        for (int i = 0; i < inputArrLength; i++)
+        {
+            outputArry[i] = movingAvgCal(movingWindowArr, &sum, pos, movingWindow, inputArr[i]);
+            pos++;
+            if (pos >= movingWindow)
+            {
+                pos = 0;
+            }
+        }
+    }
+
+    DECLDLL void FreeObject(void* obj)
+    {
+        free(obj);
+    }
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ CAPI/MovingAvgAlgo/movingAvgLib.h
@@ -0,0 +1,39 @@
+//Moving Average Algorithm DLL
+#ifndef MOVINGAVG_LIB_H
+#define MOVINGAVG_LIB_H
+
+#ifdef __cpluscplus
+extern "C" {
+#endif // __cpluscplus
+
+#if defined (_MSC_VER)
+#ifdef MOVINGAVG_EXPORT
+#define DECLDLL __declspec(dllexport)
+#else
+#define DECLDLL __declspec(dllexport)
+#endif
+#endif
+
+    DECLDLL typedef struct
+    {
+        int Size;
+        int* InputArrPtr;
+        int Window;
+        int* MovingWindowArr;
+
+    }MovingAvgIn;
+
+    DECLDLL typedef struct
+    {
+        double* OutputArrPtr;
+    }MvoingAvgOut;
+
+    DECLDLL void MovingAvgArr(int* inputArr, int inputArrLength, int* movingWindowArr, int movingWindow, double* outputArry);
+
+    DECLDLL void FreeObject(void* obj);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
